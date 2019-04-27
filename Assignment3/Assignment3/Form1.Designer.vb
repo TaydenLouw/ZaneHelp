807004f1a3e36d2d91bea5d5c07c60473eed2560
Tayden Louw <tayden@bbd.co.za>
2019-04-27 17:08:52 +0200

    Assignment 3 added

---
+++ Assignment3/Assignment3/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.ListBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1040, 367)
+        Me.SplitContainer1.SplitterDistance = 210
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(826, 367)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1040, 367)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Assignment3/Assignment3/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports System.Linq
+Public Class Form1
+    'Assume only 12 months at a time
+    Private salesFigures As Double(,) = New Double(3, 13) {}
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fileReader = New StreamReader("data.csv")
+        Dim headings = fileReader.ReadLine().Split(",")
+        Dim index = 0
+        While (Not fileReader.EndOfStream And index < 12)
+            Dim monthlySales = fileReader.ReadLine().Split(",")
+            salesFigures(0, index) = Double.Parse(monthlySales(1))
+            salesFigures(1, index) = Double.Parse(monthlySales(2))
+            salesFigures(2, index) = Double.Parse(monthlySales(3))
+        End While
+        For province = 0 To 2
+            For monthOfYear = 0 To 11
+                salesFigures(province, 12) += salesFigures(province, monthOfYear)
+            Next
+        Next
+        Dim totalSales = salesFigures(0, 12) + salesFigures(1, 12) + salesFigures(2, 12)
+        ListBox1.Items.Add("Sales report")
+        ListBox1.Items.Add(String.Format("{0,-20}{1,20}{2,20}{3,20}", "Value", "Kwa-Zulu Natal", "Gauteng", "Western Cape"))
+        ListBox1.Items.Add(String.Format("{0,-20}{1,20:C}{2,20:C}{3,20:C}", "Total", salesFigures(0, 12), salesFigures(1, 12), salesFigures(2, 12)))
+        ListBox1.Items.Add(String.Format("{0,-20}{1,20:P0}{2,20:P0}{3,20:P0}", "Percentage", (salesFigures(0, 12) / totalSales), (salesFigures(1, 12) / totalSales), (salesFigures(2, 12) / totalSales)))
+    End Sub
+End Class
